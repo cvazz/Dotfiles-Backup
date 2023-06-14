e309847ea2662b2d4eb46319f83f3bde92672915
Sebastian Bielfeldt <50203100+cvazz@users.noreply.github.com>
2023-06-14 10:44:56 +0200

    fix stash pop mishap

--- nvim/.config/nvim/lua/cvaz/packer.lua
+++ nvim/.config/nvim/lua/cvaz/packer.lua
@@ -4,18 +4,18 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
+    -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
 
 
-    use( 'mbbill/undotree') -- undotree
+    use('mbbill/undotree')  -- undotree
 
     -- File Search
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
         -- or                            , branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
+        requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
     -- quick commenting
@@ -23,19 +23,22 @@ return require('packer').startup(function(use)
 
     -- eye candie
     -- use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
-    use {'itchyny/lightline.vim'}
-    use {'gruvbox-community/gruvbox'}
+    use { 'itchyny/lightline.vim' }
+    use { 'gruvbox-community/gruvbox' }
 
 
     -- Integrate tmux & nvim
-    use ('christoomey/vim-tmux-navigator', {lazy = false})
+    use('christoomey/vim-tmux-navigator', { lazy = false })
 
-    -- Integrate tmux & nvim
+    -- Jupyter Notebooks in Neovim. Currently only without tmux
     -- use {'luk400/vim-jukit'}
 
     -- Syntax Highlighting
 
-    use( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+
+
+
 
 
     -- AI code Completion
@@ -85,22 +88,22 @@ return require('packer').startup(function(use)
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v1.x',
         requires = {
-        -- LSP Support
-        {'neovim/nvim-lspconfig'},             -- Required
-        {'williamboman/mason.nvim'},           -- Optional
-        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },       -- Required
+            { 'williamboman/mason.nvim' },     -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'},         -- Required
-        {'hrsh7th/cmp-nvim-lsp'},     -- Required
-        {'hrsh7th/cmp-buffer'},       -- Optional
-        {'hrsh7th/cmp-path'},         -- Optional
-        {'saadparwaiz1/cmp_luasnip'}, -- Optional
-        {'hrsh7th/cmp-nvim-lua'},     -- Optional
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },   -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'hrsh7th/cmp-buffer' }, -- Optional
+            { 'hrsh7th/cmp-path' },   -- Optional
+            { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+            { 'hrsh7th/cmp-nvim-lua' }, -- Optional
 
-        -- Snippets
-        {'L3MON4D3/LuaSnip'},             -- Required
-        {'rafamadriz/friendly-snippets'}, -- Optional
-  }
-}
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },       -- Required
+            { 'rafamadriz/friendly-snippets' }, -- Optional
+        }
+    }
 end)
